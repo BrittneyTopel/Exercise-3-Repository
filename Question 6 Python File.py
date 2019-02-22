214d1275ab1ca368723ca76b90576ee27dce19a4
BrittneyTopel <47437814+BrittneyTopel@users.noreply.github.com>
2019-02-21 18:49:19 -0700

    Add files via upload

---
+++ Question 6 Python File.py
@@ -0,0 +1,19 @@
+import arcpy
+
+arcpy.env.workspace = r'D:\GIS 610 _ Assignment 3\Exercise 3.gdb\Exercise 3.gdb'
+inFeatures = 'CallsforService'
+fieldname = 'Crime_Explanation'
+field_type = 'text'
+
+arcpy.AddField_management(inFeatures,fieldname,"TEXT")
+
+featureClass = r'D:\GIS 610 _ Assignment 3\Exercise 3.gdb\Exercise 3.gdb\CallsforService'
+FieldNames = ['CFSType','Crime_Explanation']
+
+with arcpy.da.UpdateCursor(featureClass, FieldNames) as cursor:
+	for x in cursor:
+		if x[0] == ('Burglary Call'):
+			x[1] = 'This is a burglary'
+			cursor.updateRow(x)
+
+			print('Finished')
